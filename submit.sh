15c07659bd44e0cf39709de438edd0114e806367
Tom Wilson <61354833+TomMakesThings@users.noreply.github.com>
2025-08-01 12:05:23 +0100

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 #SBATCH --partition=long
-#SBATCH --job-name=pseudo_snake
+#SBATCH --job-name=pseudobulk
 #SBATCH --ntasks=8
 #SBATCH --mem=100G
 #SBATCH --time=01-00:00:00 # DAYS-HOURS:MINUTES:SECONDS
-#SBATCH --mail-user=tom.wilson@imm.ox.ac.uk
 #SBATCH --output=slurm_debug/%j_%x.out
 #SBATCH --error=slurm_debug/%j_%x.err
 
-source /path/to/baseenv/bin/activate upstream
-
+# Create debugging directory
 mkdir slurm_debug -p
 
-snakemake --configfile=config/config.yaml all --cores 8 --unlock
-snakemake --configfile=config/config.yaml all --cores 8 --rerun-incomplete --keep-going
+# Set source for conda environment (must be configured to match user directory)
+source /path/to/baseenv/bin/activate pseudobulk
+
+CONFIG_FILE=config/config.yaml
+
+# Unlock workflow in case previous run failed
+snakemake --configfile="$CONFIG_FILE" all --cores "$SLURM_NTASKS" --unlock
+# Run workflow
+snakemake --configfile="$CONFIG_FILE" all --cores "$SLURM_NTASKS" --rerun-incomplete --keep-going
